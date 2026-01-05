95497d914a8836764421a044698a2b86112f26af
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-04 22:08:58 -0500

    Primer commit - Estructura Base

---
+++ src/main/java/com/hconline/permissionmanager/PermissionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hconline.permissionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionManagerApplication.class, args);
+	}
+
+}
