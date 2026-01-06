6fc1535b12f69562cc0c9aa0146d5f710270e7bd
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-06 18:26:48 -0500

    feat: Agrega excepciones BusinessValidationException y ResourceNotFoundException

---
+++ src/main/java/com/hconline/permissionmanager/exception/BusinessValidationException.java
@@ -0,0 +1,7 @@
+package com.hconline.permissionmanager.exception;
+
+public class BusinessValidationException extends RuntimeException {
+    public BusinessValidationException(String message) {
+        super(message);
+    }
+}
